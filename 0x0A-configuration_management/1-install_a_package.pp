b51f8566f2328ad758c762feafce79bee13cf46e
FreemanBoss <ibrahimgbagba800@gmail.com>
2024-06-28 13:41:41 +0100

    task 1 reviewed

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,5 @@
-# Installing flask from pip3 with Puppet
-
-package { 'flask':
-  ensure   => '2.1.0',
-  name     => 'flask',
-  provider => 'pip3'
+# install puppet-lint
+package{'puppet-lint':
+ensure   => '2.5.0',
+provider => 'gem'
 }
