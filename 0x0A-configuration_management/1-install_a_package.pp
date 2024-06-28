06b7982df20eb51e677f776251609b77ae033339
FreemanBoss <ibrahimgbagba800@gmail.com>
2024-06-28 13:59:42 +0100

    it should work now

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -4,8 +4,17 @@
 # Install flask
 # Version must be 2.1.0
 
+package {'python3':
+  ensure   => '3.8.10',
+  provider => 'pip3',
+}
 package { 'flask':
   ensure   => '2.1.0',
   name     => 'flask',
+  provider => 'pip3',
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
   provider => 'pip3',
 }
