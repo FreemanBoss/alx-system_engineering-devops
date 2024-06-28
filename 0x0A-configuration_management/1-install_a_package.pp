1ffc1ff32277eb63e8b886d930e931bafb8675bf
FreemanBoss <ibrahimgbagba800@gmail.com>
2024-06-28 13:37:43 +0100

    task 1 reviewed

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
-#!/usr/bin/pup
-## Using Puppet, install flask from pip3. version 2.1.0
+# Installing flask from pip3 with Puppet
 
-package {'flask':
-ensure   => '2.1.0',
-provider => 'pip3'
+package { 'flask':
+  ensure   => '2.1.0',
+  name     => 'flask',
+  provider => 'pip3'
 }
