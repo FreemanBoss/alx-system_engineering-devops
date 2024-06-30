d7d09d811f2d83671bee32e45b010266d6dac0c4
FreemanBoss <ibrahimgbagba800@gmail.com>
2024-06-30 04:41:40 +0100

    puppet task done

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,15 +2,11 @@
 include stdlib
 
 file_line { 'Turn off passwd auth':
-  ensure  => present,
-  path    => '/etc/ssh/ssh_config',
-  line    => '    PasswordAuthentication no',
-  replace => true,
+  path => '/etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no',
 }
 
 file_line { 'Delare identity file':
-  ensure  => present,
-  path    => '/etc/ssh/ssh_config',
-  line    => '     IdentityFile ~/.ssh/school',
-  replace => true,
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/school',
 }
