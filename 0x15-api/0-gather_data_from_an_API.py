a17d45585c6700061256a9de270cbb14223fa1e8
FreemanBoss <ibrahimgbagba800@gmail.com>
2024-08-02 17:05:54 +0100

    task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,10 +2,10 @@
 """ Python script that, using this REST API, for a given employee ID,
 returns information about his/her TODO list progress."""
 
+from sys import argv
 import json
 from urllib.request import urlopen
 import requests
-from sys import argv
 
 
 if __name__ == "__main__":
